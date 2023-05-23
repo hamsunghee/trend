4c7935b667e4e68ae4a4dd44a8c3bc7a94931a00
hamsunghee <rjswjswl77@naver.com>
2023-05-23 20:57:22 +0900

    good

---
+++ js/script6.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+  $(".trigger").click(function(){
+    $(this).toggleClass("active");
+    $(".gnb,.sns").slideToggle();
+  });
+
+  $(window).resize(function(){
+    
+    let w = $(window).width();
+
+    if (w>768){
+      $('.gnb,.sns').removeAttr('style');
+    }
+});
+/*     if (w>768){
+      $('.gnb,.sns').css({'display':'flex'});
+    }else{
+      $('.gnb,.sns').css({'display':'none'});
+    }
+
+  });
+ */
+
+});
